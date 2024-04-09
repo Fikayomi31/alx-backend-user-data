5eebba3026fbb8b24f0baa56ad926b0717eeba4c
Fikayomi31 <fikayoogundijo@gmail.com>
2024-04-08 17:25:04 -0700

    4. Read and filter data

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -91,3 +91,9 @@ def main() -> None:
     cursor = db.cursor()
     cursor.execute('SELECT * FROM users;')
     logger = get_logger()
+    fields = cursor.column_names
+    for row in cursor:
+        message = "".join("{}={};".format(k, v) for k, v in zip(fields, row))
+        logger.info(message.strip())
+    cursor.close()
+    db.close()
