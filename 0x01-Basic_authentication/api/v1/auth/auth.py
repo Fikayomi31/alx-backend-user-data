d4d7c903ec0ee257105fb4f1c85f7f219c333990
Fikayomi31 <fikayoogundijo@gmail.com>
2024-04-10 00:33:39 +0000

    3. Auth class

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -3,25 +3,26 @@
 from flask import Flask, request
 from typing import List, TypeVar
 
+
 class Auth:
     """Representation of the class"""
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """Returns false-path and excluded_paths 
+        """Returns false-path and excluded_paths
         Args:
             path: path
             excluded_path: excluded path
         return: Flase
         """
         return False
-    
+
     def authorization_header(self, request=None) -> str:
         """Reeturn None
         Args:
             request: None
         """
         return None
-    
+
     def current_user(self, request=None) -> TypeVar('User'):
         """Return None
         Args:
