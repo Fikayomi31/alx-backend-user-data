5547a152e84f0631778e2048041ec4319809c0e5
Fikayomi31 <fikayoogundijo@gmail.com>
2024-04-21 22:09:42 +0000

    7. Basic - Base64 part

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,8 +1,26 @@
 #!/usr/bin/env python3
 """Creating a class for BasicAuth"""
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
     """Representation of the class"""
-    pass
+
+    def __init__(self) -> None:
+        """doc doc doc"""
+        super().__init__()
+        
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """returns the Base64 part of the Authorization header
+        for a Basic Authentication
+        """
+        if authorization_header is None:
+            return None
+        if not isinstance(authorization_header, str):
+            return None
+        if not authorization_header.startswith('Basic '):
+            return None
+        token = authorization_header.split(' ')[-1]
+        return token
