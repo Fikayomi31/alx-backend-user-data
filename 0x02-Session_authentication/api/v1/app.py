21393072ff5b3ce6cedb1f3773c8a3961aee513b
Fikayomi31 <fikayoogundijo@gmail.com>
2024-04-24 16:51:25 -0700

    5. Before request

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -32,20 +32,15 @@ def before_request_func():
     authorized_list = ['/api/v1/status/',
                        '/api/v1/unauthorized/', '/api/v1/forbidden/',
                        '/api/v1/auth_session/login/']
-    if auth is None:
-        return
-    if not auth.require_auth(request.path, authorized_list):
-        return
-    if auth.authorization_header(request) is None:
-        abort(401)
-    if auth.current_user(request) is None:
-        abort(403)
-    request.current_user = auth.current_user(request)
-    if auth.session_cookie(request) is None:
-        abort(401)
-    if (auth.authorization_header(request) and
+    if auth and auth.require_auth(request.path, authorized_list):
+        if not auth.authorization_header(request):
+            abort(401)
+        if (auth.authorization_header(request) and
             not auth.session_cookie(request)):
             abort(401)
+        request.current_user = auth.current_user(request)
+        if not auth.current_user(request):
+            abort(403)
 
 @app.errorhandler(401)
 def unauthorized(error) -> str:
