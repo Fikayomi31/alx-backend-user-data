e6e1faafea39cd2ec6c24b72cbdddb1139cf6f59
Fikayomi31 <fikayoogundijo@gmail.com>
2024-04-25 15:28:37 +0000

    7. New view for Session Authentication

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -15,13 +15,12 @@ def login():
     user_email = request.form.get('email')
     user_password = request.form.get('password')
     if not user_email:
-        return jsonify(error='email missing'), 400
+        return jsonify({'error': 'email missing'}), 400
     if not user_password:
-        return jsonify(error='password missing'), 400
-    try:
-        user = User.search({'email': user_email})
-    except Exception:
-        return jsonify(error='no user found for this email'), 404
+        return jsonify({'error': 'password missing'}), 400
+    user = User.search({'email': user_email})
+    if not user:
+        return jsonify({'error': 'no user found for this email'}), 404
     for u in user:
         if u.is_valid_password(user_password):
             user_id = u.id
@@ -29,12 +28,12 @@ def login():
             session_id = auth.create_session(user_id)
             response = jsonify(u.to_json())
             response.set_cookie(getenv('SESSION_NAME'), session_id)
-        else:
-            return jsonify(error='wrong password for this email'), 401
-    return jsonify(error="no user found for this email"), 404
+            return response
+    return jsonify({'error': "wrong password"}), 404
 
 
-@app_views.route('/auth_session/logout', methods=['DELETE'], strict_slashes=False)
+@app_views.route('/auth_session/logout',
+                 methods=['DELETE'], strict_slashes=False)
 def logout():
     """New method that delete user"""
     from api.v1.app import auth
