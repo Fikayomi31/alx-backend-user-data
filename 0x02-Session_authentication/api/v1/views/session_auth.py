8da9a0ead8202dd3255510e4c9065a743db667d9
Fikayomi31 <fikayoogundijo@gmail.com>
2024-04-25 15:49:29 +0000

    amending the task 7

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -19,7 +19,7 @@ def login():
     if not user_password:
         return jsonify({'error': 'password missing'}), 400
     users = User.search({"email": email})
-    if not users or users == []:
+    if not users:
         return jsonify({"error": "no user found for this email"}), 404
     for user in users:
         if user.is_valid_password(password):
