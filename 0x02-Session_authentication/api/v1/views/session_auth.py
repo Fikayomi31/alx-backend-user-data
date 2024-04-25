307a0b956d38e20911ad5818c6f47caa3928855b
Fikayomi31 <fikayoogundijo@gmail.com>
2024-04-25 05:32:14 -0700

    7. New view for Session Authentication

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -8,7 +8,7 @@ from models.user import User
 from os import getenv
 
 
-@app_views.route('/auth_session/login' method=['POST'], strict_slashes=False)
+@app_views.route('/auth_session/login' methods=['POST'], strict_slashes=False)
 def login():
     """ Handle Login
     """
